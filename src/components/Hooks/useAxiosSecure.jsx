d376e5235eb9ab15e35818dec4ab2552f7dfd2b1
Md. Shahriar Hossain Alvi <shahriarhossainalvi@gmail.com>
2024-06-03 23:30:12 +0600

    added tourist dashboard routes

--- src/components/Hooks/useAxiosSecure.jsx
+++ src/components/Hooks/useAxiosSecure.jsx
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import useAuth from "./useAuth";
 
 
 const axiosSecure = axios.create({
@@ -6,6 +8,34 @@ const axiosSecure = axios.create({
 })
 
 const useAxiosSecure = () => {
+    const navigate = useNavigate();
+    const { logoutUser } = useAuth();
+
+      
+    //request interceptor to add authorization header for every secure call to the api
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token');
+        config.headers.authorization = `Bearer ${token}`
+        // console.log('request stopped by interceptors', token);
+        return config;
+    }, (error) => {
+        // Do something with request error
+        return Promise.reject(error);
+    })
+
+
+    axiosSecure.interceptors.response.use(function (response) {
+        //response within the range of 2xx
+        return response;
+    }, async (error) => {
+        // status that falls outside 2xx
+        const status = error.response.status;
+        if (status === 401 || status === 403) {
+            await logoutUser();
+            navigate('/');
+        }
+        return Promise.reject(error);
+    });
     return axiosSecure;
 };
 
--- src/components/Utility/Dashboard/DashboardDrawer.jsx
+++ src/components/Utility/Dashboard/DashboardDrawer.jsx
@@ -1,25 +1,32 @@
 import { TiThMenu } from "react-icons/ti";
+import { Link } from "react-router-dom";
 
 const DashboardDrawer = () => {
     return (
-        <div className="drawer lg:drawer-open bg-ttPrimary">
+        <div className="drawer lg:drawer-open">
             <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
             <div className="drawer-content flex flex-col items-start justify-start">
 
                 {/* Page content here */}
-                <label htmlFor="my-drawer-2" className="btn bg-ttPrimary border-0  hover:bg-ttSecondary text-white drawer-button lg:hidden">
+                <label htmlFor="my-drawer-2" className="btn border-0 bg-ttPrimary hover:bg-ttSecondary text-white drawer-button lg:hidden">
                     <TiThMenu className="text-xl" />
                 </label>
 
             </div>
             <div className="drawer-side">
                 <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
-                <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
-                    {/* Sidebar content here */}
-                    <li><a>Sidebar Item 1</a></li>
-                    <li><a>Sidebar Item 2</a></li>
-                </ul>
 
+                <ul className="menu p-4 w-80 min-h-full text-white bg-ttPrimary">
+                    {/* Sidebar content here */}
+                    <div className="flex flex-col text-center">
+                        <a className="text-3xl font-semibold"><span className="text-ttSecondary">T</span>rip <span className="text-ttSecondary font-medium">T</span>ailor</a>
+                        <a className="text-xs  text-ttSecondary">BANGLADESH</a>
+                    </div>
+                    <li><Link to='myProfile'>My Profile</Link></li>
+                    <li><Link to='myBookings'>My Bookings</Link></li>
+                    <li><Link to='myWishlist'>My Wishlist</Link></li>
+                    <li><Link to='RequestToAdmin'>Request to Admin</Link></li>
+                </ul>
             </div>
         </div>
     );
